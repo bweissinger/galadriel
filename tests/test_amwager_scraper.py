11d1f80116a0f858286a4f46f01454b2235ba6a7
bweissinger <weissingerb@gmail.com>
2021-09-02 16:32:40 -0500

    Rename amwager_parser to amwager_scraper

--- tests/test_amwager_scraper.py
+++ tests/test_amwager_scraper.py
@@ -7,12 +7,12 @@ from datetime import datetime, time
 from unittest.mock import MagicMock
 from freezegun import freeze_time
 
-from src import amwager_parser as amwparser
+from src import amwager_scraper as scraper
 from src import database as database
 from . import helpers
 
 RES_PATH = './tests/resources'
-YAML_PATH = path.join(RES_PATH, 'test_amwager_parsers.yml')
+YAML_PATH = path.join(RES_PATH, 'test_amwager_scraper.yml')
 yaml_vars = None
 with open(YAML_PATH, 'r') as yaml_file:
     yaml_vars = yaml.safe_load(yaml_file)
@@ -21,70 +21,70 @@ with open(YAML_PATH, 'r') as yaml_file:
 class TestPostTime(unittest.TestCase):
     def setUp(self):
         super().setUp()
-        self.local_zone = amwparser.get_localzone
-        amwparser.get_localzone = MagicMock()
-        amwparser.get_localzone.return_value = pytz.UTC
+        self.local_zone = scraper.get_localzone
+        scraper.get_localzone = MagicMock()
+        scraper.get_localzone.return_value = pytz.UTC
         return
 
     def tearDown(self):
         super().tearDown()
-        amwparser.get_localzone = self.local_zone
+        scraper.get_localzone = self.local_zone
         return
 
     def test_empty_html(self):
-        post = amwparser.get_post_time('')
+        post = scraper.get_post_time('')
         self.assertEqual(post, None)
 
     def test_html_is_none(self):
-        post = amwparser.get_post_time(None)
+        post = scraper.get_post_time(None)
         self.assertEqual(post, None)
 
     def test_no_post_time_listed(self):
         file_path = path.join(RES_PATH, 'amw_mtp_time.html')
         with open(file_path, 'r') as html:
-            post = amwparser.get_post_time(html.read())
+            post = scraper.get_post_time(html.read())
             self.assertEqual(post, None)
-            amwparser.get_localzone.assert_not_called()
+            scraper.get_localzone.assert_not_called()
 
     def test_correct_post_time(self):
         file_path = path.join(RES_PATH, 'amw_post_time.html')
         with open(file_path, 'r') as html:
-            post = amwparser.get_post_time(html.read())
+            post = scraper.get_post_time(html.read())
             expected = time(16, 15, 0, tzinfo=pytz.UTC)
             self.assertEqual(post, expected)
-            amwparser.get_localzone.assert_called_once()
-            amwparser.get_localzone.reset_mock()
+            scraper.get_localzone.assert_called_once()
+            scraper.get_localzone.reset_mock()
 
 
 class TestMTP(unittest.TestCase):
     def test_mtp_listed(self):
         file_path = path.join(RES_PATH, 'amw_mtp_time.html')
         with open(file_path, 'r') as html:
-            mtp = amwparser.get_mtp(html.read())
+            mtp = scraper.get_mtp(html.read())
             self.assertEqual(mtp, 5)
 
     def test_mtp_not_listed(self):
         file_path = path.join(RES_PATH, 'amw_post_time.html')
         with open(file_path, 'r') as html:
-            mtp = amwparser.get_mtp(html.read())
+            mtp = scraper.get_mtp(html.read())
             self.assertEqual(mtp, None)
 
     def test_empty_html(self):
-        post = amwparser.get_post_time('')
+        post = scraper.get_post_time('')
         self.assertEqual(post, None)
 
     def test_html_is_none(self):
-        post = amwparser.get_post_time(None)
+        post = scraper.get_post_time(None)
         self.assertEqual(post, None)
 
 
-class TestTrackListParsing(unittest.TestCase):
+class TestGetTrackList(unittest.TestCase):
     def test_track_list(self):
         expected = yaml_vars[
             self.__class__.__name__]['test_track_list']['expected']
         file_path = path.join(RES_PATH, 'amw_mtp_time.html')
         with open(file_path, 'r') as html:
-            tracks = amwparser.get_track_list(html.read())
+            tracks = scraper.get_track_list(html.read())
             self.assertEqual(tracks, expected)
 
 
@@ -92,20 +92,20 @@ class TestGetNumRaces(unittest.TestCase):
     def test_num_correct(self):
         file_path = path.join(RES_PATH, 'amw_mtp_time.html')
         with open(file_path, 'r') as html:
-            nums = amwparser.get_num_races(html.read())
+            nums = scraper.get_num_races(html.read())
             self.assertEqual(nums, 12)
 
     def test_closed_meet_race_nums(self):
         file_path = path.join(RES_PATH, 'amw_all_races_finished.html')
         with open(file_path, 'r') as html:
-            nums = amwparser.get_num_races(html.read())
+            nums = scraper.get_num_races(html.read())
             self.assertEqual(nums, 8)
 
     def test_no_race_nums_in_html(self):
-        nums = amwparser.get_num_races('')
+        nums = scraper.get_num_races('')
         self.assertEqual(nums, None)
 
-        nums = amwparser.get_num_races(None)
+        nums = scraper.get_num_races(None)
         self.assertEqual(nums, None)
 
 
@@ -113,21 +113,21 @@ class TestGetFocusedRaceNum(unittest.TestCase):
     def test_open_meet(self):
         file_path = path.join(RES_PATH, 'amw_mtp_time.html')
         with open(file_path, 'r') as html:
-            num = amwparser.get_focused_race_num(html.read())
+            num = scraper.get_focused_race_num(html.read())
             self.assertEqual(num, 12)
 
     def test_closed_meet(self):
         file_path = path.join(RES_PATH, 'amw_all_races_finished.html')
         with open(file_path, 'r') as html:
-            num = amwparser.get_focused_race_num(html.read())
+            num = scraper.get_focused_race_num(html.read())
             self.assertEqual(num, 8)
 
     def test_empty_html(self):
-        num = amwparser.get_focused_race_num('')
+        num = scraper.get_focused_race_num('')
         self.assertEqual(num, None)
 
     def test_none_html(self):
-        num = amwparser.get_focused_race_num(None)
+        num = scraper.get_focused_race_num(None)
         self.assertEqual(num, None)
 
 
@@ -136,7 +136,7 @@ class TestGetEstimatedPost(unittest.TestCase):
     def test_mtp_displayed(self):
         file_path = path.join(RES_PATH, 'amw_mtp_time.html')
         with open(file_path, 'r') as html:
-            est_post = amwparser.get_estimated_post(html.read())
+            est_post = scraper.get_estimated_post(html.read())
             expected = datetime(2020, 1, 1, 12, 35, tzinfo=pytz.UTC)
             self.assertEqual(est_post, expected)
 
@@ -144,7 +144,7 @@ class TestGetEstimatedPost(unittest.TestCase):
     def test_post_time_displayed_after_current_time(self):
         file_path = path.join(RES_PATH, 'amw_post_time.html')
         with open(file_path, 'r') as html:
-            est_post = amwparser.get_estimated_post(html.read())
+            est_post = scraper.get_estimated_post(html.read())
             expected = datetime(2020, 1, 1, 16, 15, tzinfo=pytz.UTC)
             self.assertEqual(est_post, expected)
 
@@ -152,16 +152,16 @@ class TestGetEstimatedPost(unittest.TestCase):
     def test_post_time_displayed_before_current_time(self):
         file_path = path.join(RES_PATH, 'amw_post_time.html')
         with open(file_path, 'r') as html:
-            est_post = amwparser.get_estimated_post(html.read())
+            est_post = scraper.get_estimated_post(html.read())
             expected = datetime(2020, 1, 2, 16, 15, tzinfo=pytz.UTC)
             self.assertEqual(est_post, expected)
 
     def test_empty_html(self):
-        est_post = amwparser.get_estimated_post('')
+        est_post = scraper.get_estimated_post('')
         self.assertEqual(est_post, None)
 
     def test_none_html(self):
-        est_post = amwparser.get_estimated_post(None)
+        est_post = scraper.get_estimated_post(None)
         self.assertEqual(est_post, None)
 
 
@@ -176,21 +176,21 @@ class TestScrapeRace(unittest.TestCase):
     def test_race_successfully_added(self):
         file_path = path.join(RES_PATH, 'amw_post_time.html')
         with open(file_path, 'r') as html:
-            result = amwparser.scrape_race(html.read(), self.meet)
+            result = scraper.scrape_race(html.read(), self.meet)
             self.assertNotEqual(result, None)
 
     def test_invalid_meet(self):
         file_path = path.join(RES_PATH, 'amw_post_time.html')
         with open(file_path, 'r') as html:
-            result = amwparser.scrape_race(html.read(), database.Meet())
+            result = scraper.scrape_race(html.read(), database.Meet())
             self.assertEqual(result, None)
 
     def test_empty_html(self):
-        result = amwparser.scrape_race('', self.meet)
+        result = scraper.scrape_race('', self.meet)
         self.assertEqual(result, None)
 
     def test_none_html(self):
-        result = amwparser.scrape_race('', self.meet)
+        result = scraper.scrape_race('', self.meet)
         self.assertEqual(result, None)
 
 
