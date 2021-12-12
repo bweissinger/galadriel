68b019b0b907bcf30eacea2a7dc9345c142d0ca0
bweissinger <weissingerb@gmail.com>
2021-12-11 22:42:28 -0600

    Modify __main__ session handling and loop controls

--- galadriel/__main__.py
+++ galadriel/__main__.py
@@ -9,6 +9,9 @@ from selenium import webdriver
 from zoneinfo import ZoneInfo
 from datetime import datetime, timedelta
 from sqlalchemy import and_
+from sqlalchemy.orm import scoped_session
+from pymonad.tools import curry
+from sqlalchemy.sql.functions import current_time
 
 from galadriel import (
     amwager_meet_prepper,
@@ -17,11 +20,10 @@ from galadriel import (
     amwager_scraper,
 )
 
-global session
 logger = logging.getLogger("MISSING_TRACKS_LOGGER")
 
 
-def _get_todays_meets_not_ignored() -> list[database.Meet]:
+def _get_todays_meets_not_ignored(session: scoped_session) -> list[database.Meet]:
     def _meet_is_today(meet: database.Meet) -> bool:
         timezone = ZoneInfo(meet.track.timezone)
         today = datetime.now(timezone).date()
@@ -43,9 +45,12 @@ def _get_todays_meets_not_ignored() -> list[database.Meet]:
     return [meet for meet in meets if _meet_is_today(meet)]
 
 
-def _get_tracks_to_scrape(amwager_meets: list[dict[str, str]]) -> list[database.Meet]:
+@curry(2)
+def _get_tracks_to_scrape(
+    session: scoped_session, amwager_meets: list[dict[str, str]]
+) -> list[database.Meet]:
     all_tracks = session.query(database.Track).all()
-    meet_already_added = [meet.track for meet in _get_todays_meets_not_ignored()]
+    meet_already_added = [meet.track for meet in _get_todays_meets_not_ignored(session)]
 
     def _get_track_in_database(meet):
         for track in all_tracks:
@@ -69,22 +74,29 @@ def _get_tracks_to_scrape(amwager_meets: list[dict[str, str]]) -> list[database.
 
 def _prep_meets(tracks_to_prep: list[database.Track]) -> None:
     currently_prepping = []
+    start_time = time.time()
     while tracks_to_prep or currently_prepping:
-        if tracks_to_prep and len(currently_prepping) < 5:
-            prepper_thread = amwager_meet_prepper.MeetPrepper(
-                tracks_to_prep.pop().id, driver.get_cookies(), cmd_args.log_dir
-            )
-            currently_prepping.append(prepper_thread)
-            prepper_thread.start()
+        for track in tracks_to_prep:
+            if len(currently_prepping) < 4:
+                prepper_thread = amwager_meet_prepper.MeetPrepper(
+                    track.id, driver.get_cookies(), cmd_args.log_dir
+                )
+                currently_prepping.append(prepper_thread)
+                prepper_thread.start()
+                tracks_to_prep.remove(track)
         for prepper in currently_prepping:
             if not prepper.is_alive():
                 currently_prepping.remove(prepper)
-        time.sleep(random.randint(3, 7))
+        current_time = time.time()
+        if current_time - start_time > 600:
+            driver.refresh()
+            start_time = current_time
+        time.sleep(15)
 
 
-def _get_todays_races_without_results() -> list[database.Race]:
+def _get_todays_races_without_results(session: scoped_session) -> list[database.Race]:
     races = []
-    for meet in _get_todays_meets_not_ignored():
+    for meet in _get_todays_meets_not_ignored(session):
         for race in meet.races:
             if not database.has_results(race):
                 races.append(race)
@@ -93,6 +105,7 @@ def _get_todays_races_without_results() -> list[database.Race]:
 
 def _watch_races(races_to_watch: list[database.Race]) -> None:
     watching = []
+    start_time = time.time()
     while watching or races_to_watch:
         dt_now = datetime.now(ZoneInfo("UTC"))
         for race in races_to_watch:
@@ -110,7 +123,11 @@ def _watch_races(races_to_watch: list[database.Race]) -> None:
         for watcher in watching:
             if not watcher.is_alive():
                 watching.remove(watcher)
-        time.sleep(random.randint(3, 7))
+        current_time = time.time()
+        if current_time - start_time > 600:
+            driver.refresh()
+            start_time = current_time
+        time.sleep(15)
 
 
 if __name__ == "__main__":
@@ -129,8 +146,6 @@ if __name__ == "__main__":
 
     database.setup_db(cmd_args.db_path, cmd_args.log_dir)
 
-    session = database.Session()
-
     profile = webdriver.FirefoxProfile()
     profile.set_preference("dom.webdriver.enabled", False)
     profile.set_preference("useAutomationExtension", False)
@@ -142,13 +157,15 @@ if __name__ == "__main__":
     input("Press Enter to continue after login...")
 
     soup = BeautifulSoup(driver.page_source, "lxml")
-
-    tracks_to_scrape = amwager_scraper.get_track_list(soup).bind(_get_tracks_to_scrape)
+    session = database.Session()
+    tracks_to_scrape = amwager_scraper.get_track_list(soup).bind(
+        _get_tracks_to_scrape(session)
+    )
 
     # All missing tracks will have already been logged when getting tracks_to_scrape
     if not cmd_args.missing_only:
         _prep_meets(tracks_to_scrape)
-        _watch_races(_get_todays_races_without_results())
+        _watch_races(_get_todays_races_without_results(session))
 
     database.Session.remove()
     driver.quit()
--- tests/test_main.py
+++ tests/test_main.py
@@ -11,7 +11,7 @@ class DBTestCase(unittest.TestCase):
     def setUp(self):
         super().setUp()
         database.setup_db("sqlite:///:memory:")
-        __main__.session = database.Session()
+        self.session = database.Session()
 
     def tearDown(self):
         database.Base.metadata.drop_all(bind=database.engine)
@@ -29,20 +29,20 @@ class TestGetTodaysMeetsInDatabase(DBTestCase):
     def setUp(self) -> None:
         super().setUp()
         dt = datetime.now(ZoneInfo("UTC"))
-        database.add_and_commit(__main__.session, database.Country(name="a"))
+        database.add_and_commit(self.session, database.Country(name="a"))
         database.add_and_commit(
-            __main__.session, database.Track(name="test", timezone="UTC", country_id=1)
+            self.session, database.Track(name="test", timezone="UTC", country_id=1)
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Track(name="test_2", timezone="UTC", country_id=1),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Track(name="test_3", timezone="UTC", country_id=1),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Meet(
                 datetime_retrieved=dt,
                 local_date=dt.date() - timedelta(days=1),
@@ -50,11 +50,11 @@ class TestGetTodaysMeetsInDatabase(DBTestCase):
             ),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Meet(datetime_retrieved=dt, local_date=dt.date(), track_id=1),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Meet(
                 datetime_retrieved=dt,
                 local_date=dt.date() - timedelta(days=1),
@@ -62,7 +62,7 @@ class TestGetTodaysMeetsInDatabase(DBTestCase):
             ),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Meet(
                 datetime_retrieved=dt,
                 local_date=dt.date() + timedelta(days=1),
@@ -70,7 +70,7 @@ class TestGetTodaysMeetsInDatabase(DBTestCase):
             ),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Meet(
                 datetime_retrieved=dt,
                 local_date=dt.date(),
@@ -84,27 +84,23 @@ class TestGetTodaysMeetsInDatabase(DBTestCase):
     @freeze_time("2020-01-01 12:30:00")
     def test_no_meets_today(self):
         database.delete_models(
-            __main__.session,
-            __main__.session.query(database.Meet).get(2),
+            self.session,
+            self.session.query(database.Meet).get(2),
         )
-        database.delete_models(
-            __main__.session, __main__.session.query(database.Meet).get(5)
-        )
-        output = __main__._get_todays_meets_not_ignored()
+        database.delete_models(self.session, self.session.query(database.Meet).get(5))
+        output = __main__._get_todays_meets_not_ignored(self.session)
         self.assertEqual(output, [])
 
     @freeze_time("2020-01-01 12:30:00")
     def test_some_tracks_have_meets_today(self):
-        output = __main__._get_todays_meets_not_ignored()
+        output = __main__._get_todays_meets_not_ignored(self.session)
         ids = [meet.id for meet in output]
         self.assertEqual(ids, [2, 5])
 
     @freeze_time("2020-01-01 12:30:00")
     def test_track_has_no_meets(self):
-        database.delete_models(
-            __main__.session, __main__.session.query(database.Meet).get(1)
-        )
-        output = __main__._get_todays_meets_not_ignored()
+        database.delete_models(self.session, self.session.query(database.Meet).get(1))
+        output = __main__._get_todays_meets_not_ignored(self.session)
         ids = [meet.id for meet in output]
         self.assertEqual(ids, [2, 5])
 
@@ -113,46 +109,46 @@ class TestGetTracksToScrape(DBTestCase):
     @freeze_time("2020-01-01 12:30:00")
     def setUp(self) -> None:
         super().setUp()
-        database.add_and_commit(__main__.session, database.Country(name="a"))
+        database.add_and_commit(self.session, database.Country(name="a"))
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Track(name="test", amwager="test", timezone="UTC", country_id=1),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Track(
                 name="test_2", amwager="test_2", timezone="UTC", country_id=1
             ),
         )
 
     def test_extra_meets_in_database(self):
-        output = __main__._get_tracks_to_scrape([{"id": "test"}])
+        output = __main__._get_tracks_to_scrape(self.session, [{"id": "test"}])
         ids = [track.id for track in output]
         self.assertEqual(ids, [1])
 
     def test_extra_meets_in_listed(self):
         output = __main__._get_tracks_to_scrape(
-            [{"id": "test"}, {"id": "test_2"}, {"id": "test_3"}]
+            self.session, [{"id": "test"}, {"id": "test_2"}, {"id": "test_3"}]
         )
         ids = [track.id for track in output]
         self.assertEqual(ids, [1, 2])
 
     def test_empty_meets_listed(self):
-        output = __main__._get_tracks_to_scrape([])
+        output = __main__._get_tracks_to_scrape(self.session, [])
         self.assertEqual(output, [])
 
     def test_empty_meets_in_database(self):
-        database.delete_models(
-            __main__.session, __main__.session.query(database.Track).all()
-        )
-        output = __main__._get_tracks_to_scrape([{"id": "test"}])
+        database.delete_models(self.session, self.session.query(database.Track).all())
+        output = __main__._get_tracks_to_scrape(self.session, [{"id": "test"}])
         self.assertEqual(output, [])
 
     def test_ignores_track(self):
-        model = __main__.session.query(database.Track).first()
+        model = self.session.query(database.Track).first()
         model.ignore = True
-        database.update_models(__main__.session, model)
-        output = __main__._get_tracks_to_scrape([{"id": "test"}, {"id": "test_2"}])
+        database.update_models(self.session, model)
+        output = __main__._get_tracks_to_scrape(
+            self.session, [{"id": "test"}, {"id": "test_2"}]
+        )
         ids = [track.id for track in output]
         self.assertEqual(ids, [2])
 
@@ -163,14 +159,14 @@ class TestGetTodaysRacesWithoutResults(DBTestCase):
         super().setUp()
         dt = datetime.now(ZoneInfo("UTC"))
         database.add_and_commit(
-            __main__.session, database.Discipline(name="thoroughbred", amwager="Tbred")
+            self.session, database.Discipline(name="thoroughbred", amwager="Tbred")
         )
-        database.add_and_commit(__main__.session, database.Country(name="a"))
+        database.add_and_commit(self.session, database.Country(name="a"))
         database.add_and_commit(
-            __main__.session, database.Track(name="test", timezone="UTC", country_id=1)
+            self.session, database.Track(name="test", timezone="UTC", country_id=1)
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Meet(
                 datetime_retrieved=dt,
                 local_date=dt.date() - timedelta(days=1),
@@ -178,11 +174,11 @@ class TestGetTodaysRacesWithoutResults(DBTestCase):
             ),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Meet(datetime_retrieved=dt, local_date=dt.date(), track_id=1),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Race(
                 datetime_retrieved=dt,
                 race_num=1,
@@ -192,7 +188,7 @@ class TestGetTodaysRacesWithoutResults(DBTestCase):
             ),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Race(
                 datetime_retrieved=dt,
                 race_num=1,
@@ -202,7 +198,7 @@ class TestGetTodaysRacesWithoutResults(DBTestCase):
             ),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Race(
                 datetime_retrieved=dt,
                 race_num=2,
@@ -212,17 +208,17 @@ class TestGetTodaysRacesWithoutResults(DBTestCase):
             ),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Runner(name="horse_a", tab=1, race_id=1, scratched=False),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Runner(
                 name="horse_b", tab=1, race_id=2, scratched=False, result=True
             ),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Runner(
                 name="horse_c",
                 tab=2,
@@ -231,12 +227,12 @@ class TestGetTodaysRacesWithoutResults(DBTestCase):
             ),
         )
         database.add_and_commit(
-            __main__.session,
+            self.session,
             database.Runner(name="horse_d", tab=1, race_id=3, scratched=False),
         )
 
     @freeze_time("2020-01-01 12:30:00")
     def test_todays_races_without_results(self):
-        output = __main__._get_todays_races_without_results()
+        output = __main__._get_todays_races_without_results(self.session)
         ids = [race.id for race in output]
         self.assertEqual(ids, [3])
